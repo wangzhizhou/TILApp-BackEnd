3ac39b881d89e06c80a9c76a37007cb7fd33f909
wangzhizhou <824219521@qq.com>
2024-02-21 02:11:42 +0800

    add tests for app

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,6 +2,7 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
+
     app.get { req async throws in
         try await req.view.render("index", ["title": "Hello Vapor!"])
     }
@@ -10,5 +11,40 @@ func routes(_ app: Application) throws {
         "Hello, world!"
     }
 
+    app.get("hello", ":name") { req async throws -> String in 
+
+        guard let name = req.parameters.get("name")
+        else {
+            throw Abort(.internalServerError) 
+        }
+        return "Hello, \(name)!"
+    }
+
+    app.post("info") { req async throws -> String in
+
+        let data = try req.content.decode(InfoData.self)
+
+        return "Hello, \(data.name)!"
+
+    }
+
+        app.post("info", "json") { req async throws -> InfoResponse in
+
+        let data = try req.content.decode(InfoData.self)
+
+        return InfoResponse(request: data)
+
+    }
+
     try app.register(collection: TodoController())
+}
+
+struct InfoData: Content {
+
+    let name: String
+}
+
+struct InfoResponse: Content {
+
+    let request: InfoData
 }
--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -2,14 +2,58 @@
 import XCTVapor
 
 final class AppTests: XCTestCase {
+
     func testHelloWorld() async throws {
+
         let app = Application(.testing)
         defer { app.shutdown() }
         try await configure(app)
+
+        try app.test(.GET, "", afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, """
+            <!doctype html>
+            <html lang="en">
+            <head>
+              <meta charset="utf-8">
+
+              <title>Hello Vapor!</title>
+            </head>
+
+            <body>
+              <h1>Hello Vapor!</h1>
+            </body>
+            </html>
+            """)
+        })
 
         try app.test(.GET, "hello", afterResponse: { res in
             XCTAssertEqual(res.status, .ok)
             XCTAssertEqual(res.body.string, "Hello, world!")
         })
+
+        try app.test(.GET, "hello/joker", afterResponse: { res in 
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "Hello, joker!")
+        })
+
+        try await app.test(.POST, "info", beforeRequest: { req async throws in 
+            let body = InfoData(name: "vapor")
+            try req.content.encode(body)
+        }, afterResponse: { res in 
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "Hello, vapor!")
+        })
+
+        try await app.test(.POST, "info/json", beforeRequest: { req async throws in 
+            let body = InfoData(name: "vapor")
+            try req.content.encode(body)
+        }, afterResponse: { res in 
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, """
+            {"request":{"name":"vapor"}}
+            """)
+        })        
+
     }
 }
