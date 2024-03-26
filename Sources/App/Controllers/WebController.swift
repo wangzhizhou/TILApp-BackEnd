71db604997d801cae99407c0f0613a44460ce032
wangzhizhou <wangzhizhou@bytedance.com>
2024-03-26 21:38:17 +0800

    display user page

--- Sources/App/Controllers/WebController.swift
+++ Sources/App/Controllers/WebController.swift
@@ -14,6 +14,8 @@ struct WebController: RouteCollection {
         
         routes.get(use: indexHandler)
         routes.get("acronyms", ":acronymID", use: acronymHandler)
+        routes.get("users", ":userID", use: userHandler)
+        routes.get("users", use: allUserHandler)
 
     }
 
@@ -35,6 +37,22 @@ struct WebController: RouteCollection {
         let context = AcronymContext(title: acronym.short, acronym: acronym, user: user)
         return try await req.view.render("acronym", context)
     }
+
+    func userHandler(_ req: Request) async throws -> View {
+        guard let user = try await User.find(req.parameters.get("userID"), on: req.db)
+        else {
+            throw Abort(.notFound)
+        }
+        let acronyms = try await user.$acronyms.get(on: req.db)
+        let context = UserContext(title: user.name, user: user, acronyms: acronyms)
+        return try await req.view.render("user", context)
+    }
+
+    func allUserHandler(_ req: Request) async throws -> View {
+        let allUser = try await User.query(on: req.db).all()
+        let context = AllUserContext(title: "All Users", users: allUser)
+        return try await req.view.render("allUser", context)
+    }
 }
 
 struct IndexContext: Encodable {
@@ -46,4 +64,15 @@ struct AcronymContext: Encodable {
     let title: String
     let acronym: Acronym
     let user: User
+}
+
+struct UserContext: Encodable {
+    let title: String
+    let user: User
+    let acronyms: [Acronym]
+}
+
+struct AllUserContext: Encodable {
+    let title: String
+    let users: [User]
 }
